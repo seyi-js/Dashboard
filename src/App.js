4453ac7ecfbfd7ac362d95927ef030b67f4493ce
ADEBAYO SAMUEL OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2020-08-12 09:45:02 +0100

    Started Design 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect} from 'react';
+import { Switch, Route } from 'react-router-dom';
 import SideBar from '../src/components/SideNav/sidenav'
 import Main from '../src/components/Main/main'
 export const App = ( props ) => {
